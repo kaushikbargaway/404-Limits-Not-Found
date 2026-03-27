e69499768685bd28c9e35f35dc105151afebe063
anirudh-3001 <anirudhpkulkarni3001@gmail.com>
2026-03-28 04:33:51 +0530

    changed the initial coins to 50.

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, unique: true, required: true, trim: true },
   email: { type: String, default: "" },
-  coins: { type: Number, default: 0 },
+  coins: { type: Number, default: 50 },
   trustScore: { type: Number, default: 50 }
 }, { timestamps: true });
 
